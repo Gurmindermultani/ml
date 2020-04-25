892b41f293d57008e1f67d4da4fa35aaeac77d54
Gurminder <gurminder@leena.ai>
2020-04-25 19:26:24 +0530

    added firt assignments

---
+++ assigments/neural_networks.py
@@ -0,0 +1,9 @@
+import numpy as np
+import plotly.express as px
+
+# print(np.linspace(-1.5, 1.5, num=10))
+# df = px.data.gapminder().query("continent == 'Oceania'")
+# fig = px.line(df, x='year', y='lifeExp', color='country')
+# fig.show()
+
+print(np.linspace(-1.5, 1.5, num=10))
